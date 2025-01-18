5776b27a5abf04f8c326161e8ac44ecca1cd765f
Vighnesh-Gaddam <vgnshgdm@gmail.com>
2025-01-18 13:00:49 +0530

    Animation 3 and some changes

--- 3. rotate/script.js
+++ 3. rotate/script.js
@@ -1,5 +1,6 @@
 gsap.to("#box", {
     x: 500, // will move 500 pixels to the right
     duration: 5, // will animate for 5 seconds
-    delay: 1 // will start the animation after 1 seconds
+    delay: 1, // will start the animation after 1 seconds
+    rotate: 360, // will rotate 360 degrees
 });
